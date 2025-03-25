077aa8ac4f441ab342acf7aeaf15a2e95ecac6a6
ismael laurentino <ismaellaurentino77@gmail.com>
2025-03-24 21:11:02 -0300

    teste

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function abrirMenu(){
+    document.getElementById("meuMenu").style.width = "250px"
+}
+
+function fecharMenu(){
+    document.getElementById("meuMenu").style.width = "0"
+}
+
+
+
